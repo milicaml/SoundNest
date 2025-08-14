600b820ba45d317d94c159c5f523af19fd29d22a
milicaml <152500785+milicaml@users.noreply.github.com>
2025-08-14 22:32:13 +0200

    first comit

---
+++ src/main/java/com/soundnest/SoundNestApplication.java
@@ -0,0 +1,13 @@
+package com.soundnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundNestApplication.class, args);
+	}
+
+}
